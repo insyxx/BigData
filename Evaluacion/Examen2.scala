73bfbe883c4ffeb55b971a334ef92b7838060813
insyxx <rochazepeda_jaime@hotmail.com>
2019-09-09 15:23:35 -0700

    Examen

--- Evaluacion/Examen2.scala
+++ Evaluacion/Examen2.scala
@@ -1,3 +1,6 @@
+var scores = Array(10,5,20,20,4, 5, 2, 25, 1)
+var scores = Array(3,4,21,36,10,28,35,5,24,42)
+
 def breakingRecords(scores:Array[Int]):=   //se inicia la funcion donde tomara un arreglo tipo entero
 {
     var score = 0           // Se agrega un contador para recorrer los espacios del arreglo
@@ -9,12 +12,14 @@ def breakingRecords(scores:Array[Int]):=   //se inicia la funcion donde tomara u
 
     for(score <- scores)   //El contador ira pasando por los espacios del arreglo
     {
-        if (score > max) {                  //si el score es mayor ala primer numero del arreglo
+        if (score > max) 
+        {                  //si el score es mayor ala primer numero del arreglo
             max = score                     //iguala la variable max a score
             higherScore = higherScore + 1   //entonces se le suma uno al puntaje alto
         }
 
-        if (score < min) {
+        if (score < min) 
+        {
             min = score
             lowerScore = lowerScore + 1
         } 
