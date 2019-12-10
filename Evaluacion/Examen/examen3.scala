7978e7a990f075c72cd7f4c20734c3a6611d46d5
insyxx <rochazepeda_jaime@hotmail.com>
2019-12-09 18:43:46 -0800

    actualizacion

--- Evaluacion/Examen/examen3.scala
+++ Evaluacion/Examen/examen3.scala
@@ -1,41 +1,29 @@
-// 1. Se importa la sesion en Spark
+//Libreria spark
 import org.apache.spark.sql.SparkSession
-
-{VectorAssembler,StringIndexer,VectorIndexer,OneHotEncoder}
+import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.feature.{VectorAssembler,StringIndexer,VectorIndexer,OneHotEncoder}
 import org.apache.spark.ml.linalg.Vectors
 import org.apache.log4j._
 
-// 2. Se minimizan los errores
+//Cara que no marque errores 
 Logger.getLogger("org").setLevel(Level.ERROR)
 
-// 3. Creamos la instancia de sesion en Spark
+// Cargamos el csv 
 val spark = SparkSession.builder().getOrCreate()
+val dataset = spark.read.option("header","true").option("inferSchema","true").csv("Wholesale customers data.csv")
 
-// 4. Se importa la libreria de Kmeans para el algoritmo
-import org.apache.spark.ml.clustering.KMeans
-
-// 5. Cargamos el dataset de Wholesale Customers Data
-val dataset = spark.read.option("header","true").option("inferSchema","true").csv("Wholesale-customers-data.csv")
-
-// 6. Seleccionamos las columnas que seran los datos "feature"
+//Selecionamos las columnas que vamos a entrenar
 val feature_data = dataset.select($"Fresh", $"Milk", $"Grocery", $"Frozen", $"Detergents_Paper", $"Delicassen")
-// 8. Se crea un nuevo objeto para las columnas de caractersiticas
 val assembler = new VectorAssembler().setInputCols(Array("Fresh", "Milk", "Grocery", "Frozen", "Detergents_Paper", "Delicassen")).setOutputCol("features")
 
-// 7. Se importa Vector Assembler para el manejo de datos
-import org.apache.spark.ml.feature
-
-// 9. Se utiliza el objetivo "assembler" para transformar "feature_data"
+//Entrenamos la data que colocamos en el vectorassembler
 val traning = assembler.transform(feature_data)
-
-// 10. Se crea el modelo con K = 3
-val kmeans = new KMeans().setK(3).setSeed(1L)
+//La data entrenada la pasamos al modelo 
+val kmeans = new KMeans().setK(2).setSeed(1L)
 val model = kmeans.fit(traning)
-
-
 // Evaluamos el cluster calculando en los errores cuadraticos
 val WSSSE = model.computeCost(traning)
 println(s"Within Set Sum of Squared Errors = $WSSSE")
-// Resultado
+//Resultado
 println("Cluster Centers: ")
 model.clusterCenters.foreach(println)
